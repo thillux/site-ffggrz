6312d441c0e7a7be2a49edb673c0c45194862301
Markus Theil <markus.theil@tu-ilmenau.de>
2020-08-13 12:47:19 +0200

    update for v2020.2
    Signed-off-by: Markus Theil <markus.theil@tu-ilmenau.de>

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_FEATURES := \
 #		selection that would be enabled by default or due to the
 #		chosen feature flags
 
-GLUON_SITE_PACKAGES := haveged iwinfo respondd-module-airtime
+GLUON_SITE_PACKAGES := iwinfo respondd-module-airtime
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
